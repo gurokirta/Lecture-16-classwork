fbcd60b063c2fa71e80bf586844a555517368b64
Guro <70147494+gurokirta@users.noreply.github.com>
2023-01-13 21:27:09 +0400

    updated

--- app.js
+++ app.js
@@ -27,5 +27,12 @@ if (num1 > num2 && num1 > num3) {
 } else if (num3 > num1 && num3 > num2) {
   console.log(num3, "არის ყველაზე მაღალი");
 }
+if (num1 < num2 && num1 < num3) {
+  console.log(num1, "არის ყველაზე დაბალი");
+} else if (num2 < num1 && num2 < num3) {
+  console.log(num2, "არის ყველაზე დაბალი");
+} else if (num3 < num1 && num3 < num2) {
+  console.log(num3, "არის ყველაზე დაბალი");
+}
 
 // console.log(num1, num2, num3);
